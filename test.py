448502d647dfa01b9d929f83dd6b2669c048441e
Atkiyama <thisisatkiyama@gmail.com>
2023-07-18 20:05:18 +0900

    修正

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import time
 import sys
 
 while True:
-    print("16:14:59.999691 -11 1111")
-          #,flush=True)
+    print("16:14:59.999691 -11 1111",flush=True)
     #sys.stdout.flush()
-    #time.sleep(3)  # 1秒待機
+    time.sleep(3)  # 1秒待機

9e86fc5020bc98b7f4b3ec2696d42822a4df1d54
Atkiyama <thisisatkiyama@gmail.com>
2023-07-20 16:37:03 +0900

    テスト追加

---
+++ inputTest.py
@@ -0,0 +1,18 @@
+
+
+
+DEVICE_CSV="device.csv"
+
+
+
+
+
+def main():
+  
+    for line in sys.stdin:
+        # 行をスペースで区切って3つの値として読み込む
+        print(line)
+        #time, rssi, uuid = line.strip().split()
+       
+if __name__ == "__main__":
+    main()

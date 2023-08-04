c067ab687e6adb0a8d3c44f15dfa59bd5455c40a
Atkiyama <thisisatkiyama@gmail.com>
2023-08-04 16:02:44 +0900

    変更

--- node/device.py
+++ node/device.py
@@ -1,10 +1,11 @@
 class Device:
     
-    def __init__(self,name,manufacture,R,G,B):
+    def __init__(self,name,manufacture,R,G,B,vias):
         self.name=name
         
         self.manufacture=manufacture
         self.R=int(R)
         self.G=int(G)        
         self.B=int(B)
+        self.vias=int(vias)
     
